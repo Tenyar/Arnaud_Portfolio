6d3120eab7039d542c88405e770fce8d1cbdb05d
arnaud kersual <arnaud.kersuall@gmail.com>
2024-06-09 23:12:21 +0200

    Portfolio V2.2

--- js/preloadImages.js
+++ js/preloadImages.js
@@ -1,14 +1,24 @@
 let images = ['../img/IUT2_v2.png', '../img/LaMartine.png', '../img/ChicoutimiUQAC.png'];
+let loadedImages = 0;
 
-function preloadImage(url) {
+function preloadImage(url, index, callback) {
     var img = new Image();
+    img.onload = function() {
+        loadedImages++;
+        if (loadedImages === images.length) {
+            callback();
+        }
+    };
     img.src = url;
 }
 
-function preloadImages(images) {
-    images.forEach(function(url) {
-        preloadImage(url);
+function preloadImages(images, callback) {
+    images.forEach(function(url, index) {
+        preloadImage(url, index, callback);
     });
 }
 
-preloadImages(images);
+preloadImages(images, function() {
+    // Code to execute after all images have finished loading
+    console.log("All images have been preloaded.");
+});
